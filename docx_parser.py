145bd3abf088e0d877f85fd013d266e1adf63f41
dt-woods <davisler@gmail.com>
2021-10-01 15:47:23 -0400

    separate common functions; fixes #5

--- docx_parser.py
+++ docx_parser.py
@@ -2,8 +2,8 @@
 #
 # docx_parser.py
 #
-# VERSION: 1.0.0
-# UPDATED: 2021-09-30
+# VERSION: 1.1.0
+# UPDATED: 2021-10-01
 #
 ##############################################################################
 # PUBLIC DOMAIN NOTICE                                                       #
@@ -30,65 +30,13 @@ import glob
 
 import docx
 
+from docx_utils import find_word_files
+from docx_utils import list_paragraph_styles
+from docx_utils import match_char_style
+
 ##############################################################################
 # FUNCTIONS
 ##############################################################################
-def find_word_files(d, k=""):
-    """
-    Name:     find_word_files
-    Inputs:   - str, file path (d)
-              - str, keyword(s) in the file to search (k)
-    Outputs:  List
-    Features: Searches the given directory for word files
-    """
-    my_search = "*%s*.docx" % (k)
-    my_files = glob.glob(os.path.join(d, my_search))
-    return my_files
-
-
-def list_paragraph_styles(d):
-    """
-    Name:     list_paragraph_styles
-    Inputs:   docx.document.Document, open word document
-    Output:   dict, style_id (keys) with name and counts (keys) found
-    Features: Returns a list of all the paragraph styles found in given doc
-    """
-    style_dict = {}
-    para_num = len(my_doc.paragraphs)
-    for i in range(para_num):
-        para = my_doc.paragraphs[i]
-        if para.style.style_id not in style_dict:
-            style_dict[para.style.style_id] = {
-                'name': para.style.name,
-                'count': 1
-            }
-        else:
-            style_dict[para.style.style_id]['count'] += 1
-    return style_dict
-
-
-def match_char_style(a, b):
-    """
-    Name:     match_char_style
-    Inputs:   - docx.text.run.Run, run from original document (a)
-              - docx.text.run.Run, run for new document (b)
-    Outputs:  None
-    Features: Returns a run object with the same font styles
-    TODO:     - go into the font setting of the paragraph run and match at
-                this level (see references)
-
-    References:
-    - https://python-docx.readthedocs.io/en/latest/api/text.html#font-objects
-    """
-    # First pass, just match "bold," "italic," and "underline" at run-level
-    if a.bold:
-        b.bold = True
-    if a.italic:
-        b.italic = True
-    if a.underline:
-        b.underline = True
-
-
 def parse_file(d, styleid):
     """
     Name:     parse_file
@@ -97,8 +45,7 @@ def parse_file(d, styleid):
     Features: Finds paragraphs of the given style and breaks it into a
               separate document
     TODO:     - include a user-defined output folder option
-              - include a user-defined output file namning scheme
-              - include character formatting (i.e., within paragraphs)
+              - include a user-defined output file naming scheme
     """
     # Initialize output document (i.e., the chapter in a book to be written)
     my_out = None
@@ -123,6 +70,7 @@ def parse_file(d, styleid):
                 match_char_style(p_run, out_r)
     # Save the last chapter
     my_out.save(my_name)
+
 
 ##############################################################################
 # MAIN
